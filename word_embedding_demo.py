ba602072ebd7413a0d98a9106f198684ecccaabe
叶宇轩 <yeyuxuan@yuxuans-mbp.home>
2021-02-22 01:36:40 +0000

    add bash script for BP

--- word_embedding_demo.py
+++ word_embedding_demo.py
@@ -6,8 +6,11 @@ sentences = ['This framework generates embeddings for each input sentence',
     'Sentences are passed as a list of string.',
     'The quick brown fox jumps over the lazy dog.']
 sentence_embeddings = model.encode(sentences)
+print(sentence_embeddings)
 
-for sentence, embedding in zip(sentences, sentence_embeddings):
-    print("Sentence:", sentence)
-    print("Embedding:", embedding)
-    print("")
+a = [1, 2]
+b = [3, 4]
+
+import numpy as np
+
+print(np.vstack((a,b)), np.vstack([a,b]))
